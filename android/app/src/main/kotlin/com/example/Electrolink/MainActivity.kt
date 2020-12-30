7e92c7f1a3d89b8c1d26d6dd852fc80e57c65a46
Jobson P Varghese <jobsonvarghese1@gmail.com>
2020-12-30 17:48:41 +0530

    Intial commit
    login screen added

---
+++ android/app/src/main/kotlin/com/example/Electrolink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Electrolink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:Electrolink/constant.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Widget _buildLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 70),
+          child: Image.asset('assets/images/logo.png',
+              height: 50, fit: BoxFit.fill),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        backgroundColor: Color(0xfff2f3f7),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: const Radius.circular(160),
+                    bottomRight: const Radius.circular(160),
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildLogo(),
+                _buildContainer(),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmailRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              // borderSide: BorderSide(
+              //   width: 0,
+              //   style: BorderStyle.none,
+              // ),
+              borderRadius: BorderRadius.circular(80),
+            ),
+            prefixIcon: Icon(
+              Icons.email,
+              color: mainColor,
+            ),
+            labelText: 'E-mail'),
+      ),
+    );
+  }
+
+  Widget _buildPasswordRow() {
+    return Padding(
+      padding: EdgeInsets.all(12),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(80),
+          ),
+          prefixIcon: Icon(
+            Icons.lock,
+            color: mainColor,
+          ),
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 1.4 * (MediaQuery.of(context).size.height / 20),
+          width: 5 * (MediaQuery.of(context).size.width / 10),
+          margin: EdgeInsets.only(bottom: 10),
+          child: RaisedButton(
+            elevation: 5.0,
+            color: mainColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            onPressed: () {},
+            child: Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: MediaQuery.of(context).size.height / 40,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildContainer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height / 30,
+                      ),
+                    ),
+                  ],
+                ),
+                _buildEmailRow(),
+                _buildPasswordRow(),
+                _buildLoginButton(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
